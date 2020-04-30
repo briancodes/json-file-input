f821a439f0e498cd3caa5adc341d4fe90cabb41f
briancodes <15702512+briancodes@users.noreply.github.com>
2020-04-30 21:49:52 +0100

    Testing utils (without jsdom access so using global)

--- src/utils/utils.spec.ts
+++ src/utils/utils.spec.ts
@@ -1,29 +1,65 @@
-import { format } from "./utils";
+import { JsonFileProcessor } from "./utils";
 
 /* 
-    Jest FileReader Mock
-    - https://github.com/Shopify/quilt/issues/303
-    Jest General Mocks
-    - https://jestjs.io/docs/en/jest-object#jestspyonobject-methodname
+    Stencil Unit Testing
+    - https://medium.com/@tally_b/unit-testing-stenciljs-1-0-c4e902a4e63c
+
+    Manual Mocks - import mock before the file to be tested
+    - https://jestjs.io/docs/en/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
 */
-describe("format", () => {
-    it("returns empty string for no names defined", () => {
-        expect(format(undefined, undefined, undefined)).toEqual("");
+
+type FileWithResult = Partial<File> & { result: string };
+
+const generateFiles = (count, type = "application/json"): FileWithResult[] => {
+    if (count <= 0) return [];
+    return new Array(count).fill(0).map((_, index) => {
+        const file: FileWithResult = {
+            name: "" + index,
+            type,
+            result: JSON.stringify({
+                name: "" + index,
+            }),
+        };
+        return file;
+    });
+};
+
+const createMockFileReader = (files: FileWithResult[]) => {
+    class MockFileReader {
+        private callback: Function;
+
+        addEventListener = jest.fn((type: string, callback: Function) => {
+            this.callback = callback;
+        });
+
+        readAsText(file: File) {
+            const targetFile = files.find((f) => f.name === file.name);
+            const result: Partial<ProgressEvent<FileReader>> = {
+                target: {
+                    result: targetFile.result,
+                } as FileReader,
+            };
+            setTimeout(() => {
+                this.callback(result);
+            });
+        }
+    }
+
+    return MockFileReader;
+};
+
+describe("JsonFileProcessor", () => {
+    const originalFileReader = (global as any).FileReader; // probably undefined
+
+    afterEach(() => {
+        (global as any).FileReader = originalFileReader;
     });
 
-    it("formats just first names", () => {
-        expect(format("Joseph", undefined, undefined)).toEqual("Joseph");
-    });
-
-    it("formats first and last names", () => {
-        expect(format("Joseph", undefined, "Publique")).toEqual(
-            "Joseph Publique"
-        );
-    });
-
-    it("formats first, middle and last names", () => {
-        expect(format("Joseph", "Quincy", "Publique")).toEqual(
-            "Joseph Quincy Publique"
-        );
+    it("todo", async () => {
+        const files = generateFiles(3);
+        (global as any).FileReader = createMockFileReader(files) as any;
+        const processor = new JsonFileProcessor();
+        const result = await processor.process(files as File[]);
+        expect(result.length).toBe(3);
     });
 });
